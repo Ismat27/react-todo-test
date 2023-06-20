fbf546c906918695667c303d1aeb54928d18710d
ismat27 <adegbiteismail27@gmail.com>
2023-06-20 11:11:49 +0100

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import Nav from "./components/Nav";
-import TodoList from "./routes/TodoList";
-import AddTask from "./routes/AddTask";
-import EditTask from "./routes/EditTask";
+import { AnimatePresence } from "framer-motion";
+import AllRoutes from "./routes/AllRoutes";
 
 function App() {
   return (
@@ -13,11 +12,9 @@ function App() {
       <ToastContainer />
       <BrowserRouter>
         <Nav />
-        <Routes>
-          <Route path="/" element={<TodoList />} />
-          <Route path="/add-task" element={<AddTask />} />
-          <Route path="/:taskId/update" element={<EditTask />} />
-        </Routes>
+        <AnimatePresence mode="wait">
+          <AllRoutes />
+        </AnimatePresence>
       </BrowserRouter>
     </>
   );
